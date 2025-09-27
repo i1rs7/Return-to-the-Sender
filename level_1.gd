81d4aac2595b4a6c10c2e4a94ece7e390e671947
i1rs7 <c27im@dalton.org>
2025-09-27 17:26:21 -0400

    death

--- level_1.gd
+++ level_1.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -8,14 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	get_tree().reload_current_scene()
-	print('hit')
-	
-
-
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	get_tree().reload_current_scene()
-	print('hit 2')
