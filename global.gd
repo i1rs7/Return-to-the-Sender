56b30b94888839d3721f2471241227ce7c287117
i1rs7 <163767002+i1rs7@users.noreply.github.com>
2025-10-12 23:43:05 -0400

    fix globals

--- global.gd
+++ global.gd
@@ -2,7 +2,7 @@ extends Node
 
 @onready var max_jumps = 2
 @onready var max_dash = 1
-@onready var lives_left = 1
+@onready var lives_left = 3
 @onready var cures_obtained = 0
 
 func _ready() -> void:
