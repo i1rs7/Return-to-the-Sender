b83a2a28fff715546c412b9a007f1d7c588f1531
i1rs7 <163767002+i1rs7@users.noreply.github.com>
2025-10-12 23:02:45 -0400

    edits

--- background_music.gd
+++ background_music.gd
@@ -1,7 +1,6 @@
 extends AudioStreamPlayer
 
 const levels_music = preload("res://Assets/soundEffects/Clement Panchout _ Journey _ 2017.wav")
-const jump = preload("res://Assets/soundEffects/pixel-jump-319167.mp3")
 
 func play_music(music: AudioStream, volume = -20.0):
 	if stream == music: 
